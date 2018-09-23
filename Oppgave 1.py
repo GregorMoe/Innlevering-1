da47b73df8315f7a044dcd877b593573534abc0a
Gregor Moe <moethegregor@gmail.com>
2018-09-23 15:13:07 +0200

    test

---
+++ Oppgave 1.py
@@ -0,0 +1,12 @@
+import math
+
+pH = float(input("Antall H+-ioner i mol per liter: "))
+pH = math.log10(pH) * -1
+if pH >= 0 and pH < 7:
+    print("Løsningen har pH " + str(pH) + " og er dermed sur.")
+elif pH == 7:
+    print("Løsningen har pH 7 og er dermed nøytral.")
+elif pH > 7 and pH <= 14:
+    print("Løsningen har pH " + str(pH) + " og er dermed basisk.")
+
+print("test")
