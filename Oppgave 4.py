eb958ab51b0beb62e55711839ad477ff750a0da9
Gregor Moe <moethegregor@gmail.com>
2018-09-25 22:26:31 +0200

    blank

--- Oppgave 4.py
+++ Oppgave 4.py
@@ -5,5 +5,5 @@ def fakultet(n): #først lager vi en funksjon som ganger sammen alle heltallene m
         return n * fakultet(n-1)
 try: #her bruker vi en try slik at programmet ikke stopper hvis det kommer en feilmelding
     print("Fakulteten er", fakultet(int(input("Hva vil du beregne fakulteten av? "))))
-except ValueError: #hvis programmet får en ValueError (f.eks. hvis den prøver å konvertere et desimaltall eller en bokstav til et heltall) skal den gjøre:
+except ValueError: #hvis en ValueError (f.eks. hvis den prøver å gjøre et desimaltall eller en bokstav til et heltall)
     print("Man kan kun beregne fakulteten av naturlige tall. ")
